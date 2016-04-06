d2c2f5886556a8b88f02c8644dc2ed106a2f9ce5
redsheep <codingsouls@gmail.com>
2016-04-07 00:24:59 +0800

    fix processorbar

--- components/processbar.js
+++ components/processbar.js
@@ -12,12 +12,11 @@ ProcessBar = function (game, x, y, width, height, type) {
 	this._bgFrame='rsgui-processbar-bg';
 	this._btnFrame='rsgui-processbar-btn';
 	this._downoffset=0;
-	this._autofit=false;
 
 	this.onChange =new Phaser.Signal();
 };
 ProcessBar.prototype = Object.create(GUIObject.prototype);
-ProcessBar.prototype.constructor = ScrollBar;
+ProcessBar.prototype.constructor = ProcessBar;
 ProcessBar.prototype.draw=function(){
 	var b=this._border;
 	var r=this._radius;
@@ -32,13 +31,15 @@ ProcessBar.prototype.draw=function(){
 		this._bmd.ctx.fill();
 		this._bmd.ctx.strokeBorder(b);
 		//draw scroll bar button
-		this._bmd.ctx.fillStyle = this._buttonColor;
-		if(this._scrolltype=='vertical'){
-			this._bmd.ctx.roundRect(b+r/4, b, w-2*b-r/2, p+b+r/4, r/2, true);
-		}else{
-			this._bmd.ctx.roundRect(b, b+r/4, p+b+r/4, h-2*b-r/2, r/2, true);
+		if(p>0){
+			this._bmd.ctx.fillStyle = this._buttonColor;
+			if(this._scrolltype=='vertical'){
+				this._bmd.ctx.roundRect(b+r/4, b+r/4, w-2*b-r/2, p+b+r/4, r/2, true);
+			}else{
+				this._bmd.ctx.roundRect(b+r/4, b+r/4, p-b-r/4, h-2*b-r/2, r/2, true);
+			}
+			this._bmd.ctx.fill();
 		}
-		this._bmd.ctx.fill();
 	}else{
 		//draw scroll bar and button with texture
 		var bgtexture=this._texture['bg'];
@@ -56,21 +57,22 @@ ProcessBar.prototype.getType=function(){
 	return 'processbar';
 }
 ProcessBar.prototype.setTheme=function(theme){
+
 	GUIObject.prototype.setTheme.call(this,theme);
 	this._buttonColor=theme.button;
 }
 ProcessBar.prototype.getValue=function(){
 	if(this._scrolltype=='vertical')
-		maxposition=this.height;//-this._buttonheight;
+		maxposition=this.height-this._radius/2-this._border;
 	else
-		maxposition=this.width;//-this._buttonheight;
+		maxposition=this.width-this._radius/2-this._border;
 	return (this._position/maxposition)*(this._range.max-this._range.min)+this._range.min;
 }
 ProcessBar.prototype.setValue=function(value){
 	if(this._scrolltype=='vertical')
-		maxposition=this.height;//-this._buttonheight;
+		maxposition=this.height-this._radius/2-this._border;
 	else
-		maxposition=this.width;//-this._buttonheight;
+		maxposition=this.width-this._radius/2-this._border;
 	this._position=((value-this._range.min)/(this._range.max-this._range.min))*maxposition;
 }
 ProcessBar.prototype.setValueRange=function(minvalue,maxvalue){
